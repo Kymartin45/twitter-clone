8049d15d78f1cd3aa6750d9bfbd29006738b99e4
Kyle Martin <theboi45@gmail.com>
2021-07-23 10:15:29 -0700

    Post request and data validtion function added

--- server/index.js
+++ server/index.js
@@ -10,22 +10,33 @@ app.use(express.json()); // Parses incoming requests
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Tweedlelee'
+        message: 'Yo this is a test'
     });
 });
 
 // Checks to see if user & post content is typed
 // Will have to do string validation for this?
+function checkPost(tweedle) {
+    return tweedle.name && tweedle.name + "" !== "" &&
+    tweedle.content && tweedle.content + "" !== "";
+}
 
 app.post('/tweedles', (req, res) => {
     // console.log(req.body)
+    if (checkPost(req.body)) {
         const tweedle = {
-            name: req.body.name,
-            content: req.body.content,
+            name: req.body.name + "",
+            content: req.body.content + "",
             timestamp: new Date()
         };
         console.log(tweedle);
-    });
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Please type a name and post'
+        });
+    }
+});
 
 app.listen(5000, () => {
     console.log('Listening on :5000');
