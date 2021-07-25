b9c35c3ddbbdaf139a03b0265c2c20f953dc5f61
Kyle Martin <theboi45@gmail.com>
2021-07-24 19:44:37 -0700

    Function --> isValidTweedle

--- server/index.js
+++ server/index.js
@@ -14,35 +14,25 @@ app.get('/', (req, res) => {
     });
 });
 
-// Checks to see if user & post content is typed
-// Will have to do string validation for this?
-function checkPost(tweedle) {
+function isValidTweedle(tweedle) {
     return tweedle.name && tweedle.name + "" !== "" &&
     tweedle.content && tweedle.content + "" !== "";
-}
+};
 
 app.post('/tweedles', (req, res) => {
-    if(!checkPost(req.body)) {
-        res.status(422)
+    if(!isValidTweedle(req.body)) {
+        res.status(422);
         res.json({
             message: 'Please type name and post'
         });
     }
     const {name, content} = req.body;
-    const timestamp = new Date().getDay;
+    const timestamp = new Date();
     const tweedle = {name, content, timestamp}; 
 
     console.log(tweedle);
-    // if (checkPost(req.body)) {
-    //     const tweedle = {
-    //         name: req.body.name + "",
-    //         content: req.body.content + "",
-    //         timestamp: new Date()
-    //     };
-    //     console.log(tweedle);
-    // } 
 });
 
 app.listen(5000, () => {
     console.log('Listening on :5000');
-})
+});
